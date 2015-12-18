38d6cbe5bce40cd95ac445da19cad72407595ba6
vidaaudrey <audrey@33road.com>
2015-12-17 17:37:30 -0800

    server: update config to use environment variables

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,8 @@
 // export NODE_ENV=development
 var env = process.env.NODE_ENV  || 'development';
 module.exports = require('./'+ env + '.json');
+module.exports.env = {
+  PORT: process.env.PORT,
+  
+}
 
